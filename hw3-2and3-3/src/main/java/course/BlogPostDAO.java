9a8b1f28931d9c18624765455f404510cbaf1acf
drei01 <mcreidie@hotmail.com>
2014-01-21 16:58:33 +0000

    add homework 3.2 and 3.3

---
+++ hw3-2and3-3/src/main/java/course/BlogPostDAO.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright (c) 2008 - 2013 10gen, Inc. <http://10gen.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package course;
+
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.DBObject;
+
+public class BlogPostDAO {
+    DBCollection postsCollection;
+
+    public BlogPostDAO(final DB blogDatabase) {
+        postsCollection = blogDatabase.getCollection("posts");
+    }
+
+    // Return a single post corresponding to a permalink
+    public DBObject findByPermalink(String permalink) {
+
+        DBObject post = null;
+        
+        post = postsCollection.findOne(new BasicDBObject("permalink", permalink));
+
+        return post;
+    }
+
+    // Return a list of posts in descending order. Limit determines
+    // how many posts are returned.
+    public List<DBObject> findByDateDescending(int limit) {
+
+        List<DBObject> posts = null;
+        // XXX HW 3.2,  Work Here
+        // Return a list of DBObjects, each one a post from the posts collection
+        posts = postsCollection.find().sort(new BasicDBObject("date", -1)).toArray();
+
+        return posts;
+    }
+
+
+    public String addPost(String title, String body, List tags, String username) {
+
+        System.out.println("inserting blog entry " + title + " " + body);
+
+        String permalink = title.replaceAll("\\s", "_"); // whitespace becomes _
+        permalink = permalink.replaceAll("\\W", ""); // get rid of non alphanumeric
+        permalink = permalink.toLowerCase();
+
+
+        BasicDBObject post = new BasicDBObject()
+        .append("title", title)
+        .append("author", username)
+        .append("permalink", permalink)
+        .append("body", body)
+        .append("tags", tags)
+        .append("comments", Collections.emptyList())
+        .append("date", new Date());
+        
+        postsCollection.save(post);
+
+
+        return permalink;
+    }
+
+
+
+
+   // White space to protect the innocent
+
+
+
+
+
+
+
+
+    // Append a comment to a blog post
+    public void addPostComment(final String name, final String email, final String body,
+                               final String permalink) {
+
+    	DBObject comment = new BasicDBObject()
+    			.append("author", name)
+    			.append("email", email)
+    			.append("body", body);
+    	
+        postsCollection.update(new BasicDBObject("permalink", permalink), 
+        		new BasicDBObject("$push", new BasicDBObject("comments", comment)));
+
+    }
+
+
+}
